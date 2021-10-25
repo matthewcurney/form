8d8673ef0c7da48b3821ed9d413e044154757c5d
Matt Curney <matthewcurney@gmail.com>
2021-10-25 16:13:23 -0500

    Update reparo.js

--- reparo.js
+++ reparo.js
@@ -1,36 +1,87 @@
-jQuery('head').append('<meta content="width=device-width, initial-scale=1" name="viewport" />');
+var labelMaker = {
+    'address1': 'Address',
+    'address2': 'Address',
+    'home': 'Home Phone',
+    'work': 'Work Phone',
+    'ssn': 'Last 4 of SSN',
+    'bday': 'Birthday',
+    'eSchool': 'Employer / School Name',
+    'misc': 'Misc/Guardian'
+};
 
-jQuery('h1').prepend('<img src="https://wallericheyecare.com/wp-content/uploads/2020/07/big-wallerich-eye-care.png" />');
-
-jQuery("input:not([type=hidden]), select").each(function () {
-    // wrap each one in a label 
-
-    // get name and add it to parent td
-    var name = jQuery(this).attr('name');
-    if (typeof name !== 'undefined') {
-        name = name.toLowerCase();
-        console.log(name);
-        jQuery(this).closest('td').addClass(name);
-    }
-    // wrap each input in a label
-    jQuery(this).wrap("<label>"+name+"</label>");
-});
-
-
-// label and placeholder edits based on field 
-jQuery(".first").find('input').attr("placeholder", "Enter your first name...");
-jQuery(".last").find('input').attr("placeholder", "Enter your last name...");
-
-jQuery('td:not([class])').remove();
-jQuery('.title').remove();
-
-// if first name table
-if(jQuery('table').find('.first').length !== 0) {
-    console.log(jQuery(this));
+var placeholderMaker = {
+    'first': 'Enter your first name...',
+    'last': 'Enter your last name...'
 }
-jQuery.ajax({
-    url: "https://raw.githubusercontent.com/matthewcurney/form/main/reparo.css",
-    success: function(data) {
-        jQuery("<style>").appendTo("head").html(data);
-    }
+
+jQuery(document).ready(function () {
+
+    var name = '';
+    var label = '';
+    var placeholder = '';
+
+    jQuery("input:not([type=hidden], [type=button]), select").each(function () {
+
+        // get input name
+        name = jQuery(this).attr('name');
+
+        // if it has one proceed and make it lowercase
+        if (typeof name !== 'undefined') {
+
+            name = name.toLowerCase();
+
+            if (jQuery(this).is(':radio') || jQuery(this).is(':checkbox') || name.startsWith('mc_')) {
+                // keep as is and add class to parent tr
+                if(name.startsWith('mc_')){
+                    jQuery(this).closest('tr').addClass('keep');
+                }
+                if(jQuery(this).is(':radio') ){
+                    jQuery(this).closest('tr').addClass('radio');
+                }
+                if(jQuery(this).is(':checkbox')){
+                    jQuery(this).closest('tr').addClass('checkbox');
+                }
+
+                // this is because radio buttons are alreay label wrapped, and checkboxes/ids are too custom to change
+
+            } else {
+                // add class to parent td
+                jQuery(this).closest('td').addClass(name);
+
+                // then get label and placeholder
+                if (name in labelMaker) {
+                    label = labelMaker[name];
+                    placeholder = placeholderMaker[name];
+                } else {
+                    label = name;
+                    placeholder = '';
+                }
+
+                    // put in label and placeholder
+                    jQuery(this).wrap("<label>" + label + "</label>");
+                    jQuery(this).attr("placeholder", placeholder);
+            }
+
+        }
+
+    });
+
+
+    jQuery('tr:not([class]) td:not([class])').remove();
+
+    // inputs that aren't needed - hide 
+    jQuery('td.title, td.other, td.alerts').remove();
+
+    jQuery('head').append('<meta content="width=device-width, initial-scale=1" name="viewport" />');
+
+    jQuery('h1').prepend('<img src="https://wallericheyecare.com/wp-content/uploads/2020/07/big-wallerich-eye-care.png" />');
+
+    // finally load in CSS
+    // jQuery.ajax({
+    //     url: "https://raw.githubusercontent.com/matthewcurney/form/main/reparo.css",
+    //     success: function (data) {
+    //         jQuery("<style>").appendTo("head").html(data);
+    //     }
+    // });
+
 });
